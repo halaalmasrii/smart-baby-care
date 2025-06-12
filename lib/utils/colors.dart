8a661ae6dee5529cced85186635d7705ba72d026
amiraBaroudi <princessba2002@gmail.com>
2025-06-13 02:00:35 +0300

    حذف تعليقات

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF81D4FA); // أزرق فاتح
-  static const Color secondary = Color(0xFFFFB74D); // برتقالي دافئ
-  static const Color background = Color(0xFFFFF3F7); // زهري خفيف
-  static const Color text = Color(0xFF4A148C); // بنفسجي داكن
-  static const Color success = Color(0xFF4CAF50); // أخضر
-  static const Color warning = Color(0xFFFFA726); // برتقالي
-  static const Color danger = Color(0xFFE57373); // أحمر فاتح
+  static const Color primary = Color(0xFF81D4FA); 
+  static const Color secondary = Color(0xFFFFB74D); 
+  static const Color background = Color(0xFFFFF3F7); 
+  static const Color text = Color(0xFF4A148C); 
+  static const Color success = Color(0xFF4CAF50); 
+  static const Color warning = Color(0xFFFFA726); 
+  static const Color danger = Color(0xFFE57373); 
 }
